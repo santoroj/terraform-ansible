0c053750b3661841210b7cd0a8b3e8ad3ff6fd4a
Joes GUEST <Joe.Santoro@raytheon.co.uk>
2023-05-31 17:37:48 +0100

    Just testing autostop

---
+++ modules/web/iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_role.name
+}
